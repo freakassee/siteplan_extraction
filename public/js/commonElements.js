a967e753d3271b68c7dcdfe54511e458cabea54c
Freaky <freakassee@gmx.de>
2015-07-08 18:38:39 +0200

    #14 - changes in *_mt.jade *_trn.jade  and *_og.jade -- all use the same base file (layout_extracted.jade) now

--- public/js/commonElements.js
+++ public/js/commonElements.js
@@ -1,5 +1,3 @@
-
-
 function createNextButton(url, left, top) {
 	var textNode = document.createTextNode('Weiter');
 	var button = document.createElement('button');
@@ -42,14 +40,48 @@ function createHiddenFormAndSubmit(imageId) {
 	form.submit();
 }
 
-function setStyleAccordingToSymbolValue(id, div,opacity,inverted) {
-	if(inverted){
-		isSymbol_values[id]=!isSymbol_values[id];
+function createOverlayDiv(index, color) {
+	//debugger;
+	margin = 8;
+	var div = document.createElement('div');
+	div.setAttribute('id', 'ovrDiv_' + index);
+	div.style.backgroundColor = color;
+	div.style.position = 'absolute';
+
+	d_left = (x_values[index] - x_values[0]) / scalingFactor + +ovr_W-margin;
+	d_top = (y_values[index] - y_values[0]) / scalingFactor + ovr_H-margin;
+
+	div.style.left = d_left + 'px';
+	div.style.top = d_top + 'px';
+
+	d_width = extracted_W2 / scalingFactor;
+	d_height = extracted_H2 / scalingFactor;
+
+	div.style.width = d_width + 'px';
+	div.style.height = d_height + 'px';
+
+	div.style.zIndex = 2;
+
+	div.addEventListener('click', function(event) {
+		onDivClick(event);
+	});
+
+	setStyleAccordingToSymbolValue(index, div, 0.5, false);
+	mainDiv.appendChild(div);
+	return div;
+}
+
+
+function setStyleAccordingToSymbolValue(id, element, opacity, inverted) {
+	if (inverted) {
+		isSymbol_values[id] = !isSymbol_values[id];
 	}
 
 	if (!isSymbol_values[id]) {
-		div.style.opacity = opacity;
+		element.style.opacity = opacity;
 	} else {
-		div.style.opacity = 0.0;
+		element.style.opacity = 0.0;
 	}
 }
+
+
